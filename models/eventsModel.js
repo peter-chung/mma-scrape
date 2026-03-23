f803f80c4fe3a286de76385615522ec3efa43a18
Peter Chung <pchung32@gmail.com>
2026-03-23 15:19:38 -0700

    remove state

--- models/eventsModel.js
+++ models/eventsModel.js
@@ -66,11 +66,6 @@ const eventSchema = new mongoose.Schema(
       default: "",
       trim: true,
     },
-    state: {
-      type: String,
-      default: "",
-      trim: true,
-    },
     country: {
       type: String,
       default: "",
